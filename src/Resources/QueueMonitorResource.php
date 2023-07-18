89c3cca214a6f69f928a21ce92feabd9ea1cd6e9
Brian <54040433+bcollins-oxford@users.noreply.github.com>
2023-07-18 10:18:46 -0400

    Update QueueMonitorResource to Fix and apply sorting
    Sorting was applied to the form and not to the table.  Removed code from $form and applied to $table

--- src/Resources/QueueMonitorResource.php
+++ src/Resources/QueueMonitorResource.php
@@ -27,8 +27,7 @@ class QueueMonitorResource extends Resource
                     ->maxLength(255),
                 Forms\Components\TextInput::make('queue')
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('started_at')
-                    ->sortable(),
+                Forms\Components\DateTimePicker::make('started_at'),
                 Forms\Components\DateTimePicker::make('finished_at'),
                 Forms\Components\Toggle::make('failed')
                     ->required(),
@@ -36,7 +35,7 @@ class QueueMonitorResource extends Resource
                     ->required(),
                 Forms\Components\Textarea::make('exception_message')
                     ->maxLength(65535),
-            ])->defaultSort('started_at', 'desc');
+            ]);
     }
 
     public static function table(Table $table): Table
@@ -62,8 +61,8 @@ class QueueMonitorResource extends Resource
                 ProgressColumn::make('progress')->label(__('filament-jobs-monitor::translations.progress'))->color('warning'),
                 Tables\Columns\TextColumn::make('started_at')
                     ->label(__('filament-jobs-monitor::translations.started_at'))
-                    ->since(),
-            ])
+                    ->since()->sortable(),
+            ])->defaultSort('started_at', 'desc')
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
