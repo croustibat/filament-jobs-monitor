bda746470b90796086479aa810fa0ddfcf2f86d2
Ondrej Musil <musil.ondrejj@gmail.com>
2023-12-20 14:03:28 +0100

    Add Czech translations

---
+++ resources/lang/cs/translations.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'breadcrumb' => 'Monitor úloh ve frontě',
+    'title' => 'Úlohy ve frontě',
+    'navigation_label' => 'Úlohy',
+    'navigation_group' => 'Systém',
+    'total_jobs' => 'Celkový počet zpracovaných úloh',
+    'execution_time' => 'Celková doba provádění',
+    'average_time' => 'Průměrná doba provádění',
+    'succeeded' => 'Úspěšně dokončeno',
+    'failed' => 'Selhalo',
+    'running' => 'Právě probíhá',
+    'status' => 'Stav',
+    'name' => 'Název',
+    'queue' => 'Fronta',
+    'progress' => 'Pokrok',
+    'started_at' => 'Čas spuštění',
+];
